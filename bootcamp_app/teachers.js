9f8501f332ac566e462bd0d91043aa17d6d44550
Manzar <manzar.hussain@telus.com>
2022-12-25 16:45:16 -0500

    parameterized queries for SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,21 +9,22 @@ const pool = new Pool({
 
 
 const queryString = `
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = $1
-ORDER BY teacher;
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+    JOIN assistance_requests ON teacher_id = teachers.id
+    JOIN students ON students.id = student_id
+    JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name IN ($1)
+  GROUP BY teacher, cohort
+  ORDER BY teacher;
 `;
-
-const cohortName = [ process.argv[2] || 'JUL02' ];
+const cohortName = [process.argv.slice(2)[0]];
 
 pool.query(queryString, cohortName)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`)
+    })
+    pool.end();
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch((err) => {console.error(err)})
